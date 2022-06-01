1174b709b0f717a785dbf9b9617a2cf3d240c866
JohnRamberger <jramberger3@gatech.edu>
2022-06-01 17:32:26 -0400

    setup hero bg image

--- client/src/Components/Hero/index.js
+++ client/src/Components/Hero/index.js
@@ -1,9 +1,12 @@
 //imports
 import "./Hero.css";
 
+import spacebg from "./dsgt background 1-2.svg";
+
 const Hero = (params) => {
     return (
         <div {...params} id="hero">
+            <img className="hero-bg-img" src={spacebg}/>
             <div className="hero-wrapper">test</div>
         </div>
     );
