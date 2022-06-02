a8b33abfa45d97808eece08856245ec43ed788b4
JohnRamberger <jramberger3@gatech.edu>
2022-06-02 17:13:29 -0400

    added image assets, setup basic hero

--- client/src/Components/DesktopNavbar/index.js
+++ client/src/Components/DesktopNavbar/index.js
@@ -1,8 +1,29 @@
 //imports
 import "./DesktopNavbar.css";
 
+import logo from "../../assets/images/dsgt/apple-touch-icon.png";
+
 const DesktopNavbar = (params) => {
-    return <div {...params} className="desktop-navbar">desktop</div>;
+    return (
+        <div {...params} className="desktop-navbar">
+            <div className="nav-wrapper">
+                {/* <img className="logo" src={logo} /> */}
+                {/* <h1 className="logo-text">Data Science at<br />Georgia Tech</h1> */}
+                <a className="nav-menuitem" href="#">
+                    About Us
+                </a>
+                <a className="nav-menuitem" href="#">
+                    Projects
+                </a>
+                <a className="nav-menuitem" href="#">
+                    Hacklytics
+                </a>
+                <a className="nav-menuitem" href="#">
+                    Bootcamp
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default DesktopNavbar;
--- client/src/Components/Hero/index.js
+++ client/src/Components/Hero/index.js
@@ -1,13 +1,20 @@
 //imports
 import "./Hero.css";
 
-import bg from "./background-design-1.svg";
+import bg1 from "./hero--export.svg";
+import herologo from "../../assets/images/dsgt/square-logo.png"
 
 const Hero = (params) => {
     return (
         <div {...params} id="hero">
-            <div className="hero-bg-img"></div>
-            <div className="hero-wrapper">test</div>
+            <div className="hero-bg"></div>
+            <img className="hero-bg-img" src={bg1} />
+            <div className="hero-wrapper">
+                <div className="hero-right">
+                    <img className="hero-logo" src={herologo} />
+                    <h1 className="hero-major">Data Science @ Georgia Tech</h1>
+                </div>
+            </div>
         </div>
     );
 };
